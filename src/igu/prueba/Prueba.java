f8dbdf832d385b808485a362de154f8aacae6157
Alfredo <alfre.silva.leon.5@gmail.com>
2020-06-16 20:25:14 -0300

    Commit #4: Constructor Ventana(String) probado!

--- src/igu/prueba/Prueba.java
+++ src/igu/prueba/Prueba.java
@@ -1,10 +1,19 @@
 package igu.prueba;
 
+import javax.swing.JFrame;
+
+import igu.ventanas.Ventana;
+
 public class Prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Ventana ventana=new Ventana();
+		ventana=new Ventana("¡Bienvenidos!");
+		
+		ventana.redimensionable(false);
+		ventana.alCerrar(JFrame.EXIT_ON_CLOSE);
+		ventana.visibilizar();
 	}
 
 }
